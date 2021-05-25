79d5f405454a7546fd2b7c74ec5f4bbb7e8f3de5
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-24 22:31:48 -0500

    Create class Board and Boardview

---
+++ main.js
@@ -0,0 +1,41 @@
+//funcion anónima para no contaminar el scope del proyecto
+(function(){
+	//slef === window
+	self.Board = function(width,height){
+		this.width = width;
+		this.height = height;
+		this.playing = false;
+		this.game_over = false;
+		this.bars = [];
+		this.ball = null;
+	}
+
+
+	self.Board.prototype = {
+		get elements(){
+			var elements = this.bars;
+			elements.push(ball);
+			return elements;
+		}
+	}
+
+})();
+
+(function(){
+	self.BoardView = function(canvas, board){
+		this.canvas = canvas;
+		this.canvas.width = board.width;
+		this.canvas.height = board.height;
+		this.board = board;
+		this.ctx = canvas.getContext("2d");
+	}
+})();
+
+
+window.addEventListener("load",main);
+
+function main(argument) {
+	var board = new Board(800, 400);
+	var canvas = document.getElementById("canvas");
+	board_view = new BoardView(canvas,board)
+}
